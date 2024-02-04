e1f76274230dd98d7042d2fc2d3f55e23d7d279b
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-02-04 11:34:47 +0600

    done

--- src/app/user/createuser/page.tsx
+++ src/app/user/createuser/page.tsx
@@ -1,10 +1,11 @@
+// 'use client'
 import CreateUserForm from "@/components/CreateUserForm";
 import React from "react";
 
 const CreateUserPage = () => {
   return (
     <div>
-      <CreateUserForm></CreateUserForm>
+      <CreateUserForm />
     </div>
   );
 };
--- src/app/user/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateProductPage = () => {
-  return <div>CreateProduct Page</div>;
-};
-
-export default CreateProductPage;
