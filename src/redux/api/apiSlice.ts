9800bf6854772a9858a09a4c1b12ed96fc7b3f05
shakhawatsalam <shakhawatsalam726@gmail.com>
2024-02-04 11:22:48 +0600

    almost done

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/",
+    baseUrl: "https://vitasoft-server.vercel.app/api",
   }),
   endpoints: () => ({}),
   tagTypes: ["user"],
